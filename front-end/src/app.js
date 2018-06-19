5e465625da848d921b6d6ed17cdb240d4b31fde2
AngelGang <yeezy.ag@gmail.com>
2018-06-19 23:53:18 +0200

    Fin du projet

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -7,23 +7,31 @@ axios.get('http://127.0.0.1:8080/news')
     var link ="";
     var date =""
 
-    for(var i = 0; i < response.data.articles.length && i < 5 ; i++ )
+    for(var i = 0; i < response.data.articles.length && i < 5; i++ )
     {    
+
          link = response.data.articles[i].url
          date = response.data.articles[i].publishedAt
             news = news + "<li id=\"listnews\" href><a href="+ link +"><p>"+ date +" : "+ response.data.articles[i].title+"</p></a></li>"
     }
-    document.getElementById("filnews").innerHTML = news 
+    document.getElementById("easyPaginate").innerHTML = news 
+
 
     var laUne ="";
     var img = "";
     var title ="";
-    for(var i = 0; i < response.data.articles.length; i++ )
+    var descrip ="";
+    for(var i = 0; i < response.data.articles.length && i < 2; i++ )
     {    
         title = response.data.articles[i].title;
         img = response.data.articles[i].urlToImage;
-        
-        laUne = "<img src=" + img + " style=\"width:100%\">"+"<div class=\"w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small\"><h3>"+ title +"</h3><\"p><b>"+ descrip +"</b></p><\"/div>"
+        descrip = response.data.articles[i].description;
+
+        laUne = "<h2 class=\"w3-wide\"> La une :</h2> <h2 class=\"w3-wide\">"+ title +"</h2><a href="+ link +"> <img src=" + img + " style=\"width:100%\"></a>"
+        +"<div class=\"w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small\">"
+        +"<p><b>"+ descrip +"<b><p>"
+        +"</div>"
     }
     document.getElementById("uneNews").innerHTML = laUne
+
 })
