68a84626e113f3a3cecc354bdf21534fe7b7bc5e
AngelGang <yeezy.ag@gmail.com>
2018-06-19 03:01:59 +0200

    tentative d'inserer une news principal dans un slidershow

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -4,12 +4,26 @@ axios.get('http://127.0.0.1:8080/news')
     console.log(response)  
 
     var news = "";
-    for(var i = 0; i < response.data.articles.length; i++ )
-    {
-        news = news + "<li href><a href="+response.data.articles[i].url +"><p>"+ response.data.articles[i].publishedAt +"  -  "+ response.data.articles[i].title+"</p></a></li>"
-        console.log(news)
+    var link ="";
+    var date =""
+
+    for(var i = 0; i < response.data.articles.length && i < 5 ; i++ )
+    {    
+         link = response.data.articles[i].url
+         date = response.data.articles[i].publishedAt
+            news = news + "<li id=\"listnews\" href><a href="+ link +"><p>"+ date +" : "+ response.data.articles[i].title+"</p></a></li>"
     }
     document.getElementById("filnews").innerHTML = news 
-    /*"<li><p>"+Date de publication - titre de larticle clickable+"</p></li>"+
-    "<li><p>"+Date de publication - titre de larticle clickable+"</p></li>"+*/
+
+    var laUne ="";
+    var img = "";
+    var title ="";
+    for(var i = 0; i < response.data.articles.length; i++ )
+    {    
+        title = response.data.articles[i].title;
+        img = response.data.articles[i].urlToImage;
+        
+        laUne = "<img src=" + img + " style=\"width:100%\">"+"<div class=\"w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small\"><h3>"+ title +"</h3><\"p><b>"+ descrip +"</b></p><\"/div>"
+    }
+    document.getElementById("uneNews").innerHTML = laUne
 })
